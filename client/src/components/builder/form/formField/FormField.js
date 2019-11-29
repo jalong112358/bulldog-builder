f124a333f257c93404b3b18671bf9526878d61a7
Jake Long <jakelong@Emms-iMac-10.local>
2019-11-29 09:37:09 -0600

    Removing image border

--- client/src/components/builder/form/formField/FormField.js
+++ client/src/components/builder/form/formField/FormField.js
@@ -3,44 +3,48 @@ import React, { Component } from "react";
 import "./FormField.css";
 
 class FormField extends Component {
-	// Sends field id to parent on change
-	handleCheck = value => {
-		this.props.onSelect(value, this.props);
-		//  console.log(value);
-	};
+  // Sends field id to parent on change
+  handleCheck = value => {
+    this.props.onSelect(value, this.props);
+    //  console.log(value);
+  };
 
-	render() {
-		let formFieldClass;
+  render() {
+    let formFieldClass;
 
-		if (this.props.isSelected === true) {
-			formFieldClass = "selected";
-		} else {
-			formFieldClass = "";
-		}
+    if (this.props.isSelected === true) {
+      formFieldClass = "selected";
+    } else {
+      formFieldClass = "";
+    }
 
-		const fieldInfo = this.props.info.map((part, index) => <li>{part}</li>);
+    const fieldInfo = this.props.info.map((part, index) => <li>{part}</li>);
 
-		return (
-			<div
-				id="form-field"
-				value={this.props.identifier}
-				className={formFieldClass}
-				onClick={() => this.handleCheck(this.props.identifier)}
-			>
-				<img src={require(`../../../../formData/images/${this.props.image}`)} />
-				<div id="field-info">
-					<span className="field-name">{this.props.name}</span>
-					<ul className="field-meta">{fieldInfo}</ul>
-					<div id="checkbox-wrapper">
-						<div id="field-checkbox">
-							<div id="field-checkbox-inner" className={formFieldClass} />
-						</div>
-						<span>Select</span>
-					</div>
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div
+        id="form-field"
+        value={this.props.identifier}
+        className={formFieldClass}
+        onClick={() => this.handleCheck(this.props.identifier)}
+      >
+        <img src={require(`../../../../formData/images/${this.props.image}`)} />
+        <div id="field-info">
+          {this.props.title === "Length" ? null : this.props.title ===
+            "Diameter" ? null : (
+            <span className="field-name">{this.props.name}</span>
+          )}
+
+          <ul className="field-meta">{fieldInfo}</ul>
+          <div id="checkbox-wrapper">
+            <div id="field-checkbox">
+              <div id="field-checkbox-inner" className={formFieldClass} />
+            </div>
+            <span>Select</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FormField;
--- client/src/components/submission/productReview/ProductReview.js
+++ client/src/components/submission/productReview/ProductReview.js
@@ -1,116 +1,116 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 // import ImageDisplay from "./imageDisplay/ImageDisplay";
-import Logo from "./logo.png";
+import Logo from "./logo.jpg";
 
 import "./ProductReview.css";
 
 class ProductReview extends Component {
-	state = {
-		buildData: []
-	};
+  state = {
+    buildData: []
+  };
 
-	componentDidUpdate() {
-		if (this.state.buildData !== this.props.buildData) {
-			this.setState({
-				buildData: this.props.buildData
-			});
-		}
-	}
+  componentDidUpdate() {
+    if (this.state.buildData !== this.props.buildData) {
+      this.setState({
+        buildData: this.props.buildData
+      });
+    }
+  }
 
-	render() {
-		let imageNumber = "";
-		let customText = "";
-		let specs;
-		if (this.state.buildData.length !== 0) {
-			imageNumber = this.state.buildData.imageNumber;
-			customText = this.state.buildData.customText;
-			specs = this.state.buildData.specs.map((part, index) => {
-				let name;
-				let title;
-				let price;
-				let info;
+  render() {
+    let imageNumber = "";
+    let customText = "";
+    let specs;
+    if (this.state.buildData.length !== 0) {
+      imageNumber = this.state.buildData.imageNumber;
+      customText = this.state.buildData.customText;
+      specs = this.state.buildData.specs.map((part, index) => {
+        let name;
+        let title;
+        let price;
+        let info;
 
-				title = part.title;
-				name = part.name;
-				price = part.price;
+        title = part.title;
+        name = part.name;
+        price = part.price;
 
-				if (part.info) {
-					info = part.info;
-				}
+        if (part.info) {
+          info = part.info;
+        }
 
-				let infoList = info.map((item, index) => {
-					return <p>{item}</p>;
-				});
+        let infoList = info.map((item, index) => {
+          return <p>{item}</p>;
+        });
 
-				return (
-					<ul style={{ width: "100%" }}>
-						<li>
-							<span className="bold-text">
-								{title}: {name}
-							</span>
+        return (
+          <ul style={{ width: "100%" }}>
+            <li>
+              <span className="bold-text">
+                {title}: {name}
+              </span>
 
-							<span>${price}</span>
-						</li>
-						<div className="spec-info">{infoList}</div>
-					</ul>
-				);
-			});
-		}
+              <span>${price}</span>
+            </li>
+            <div className="spec-info">{infoList}</div>
+          </ul>
+        );
+      });
+    }
 
-		return (
-			<div id="product-review">
-				<div className="logo-wrapper">
-					<img src={Logo} />
-				</div>
-				{this.state.buildData.length !== 0 ? (
-					<img
-						className="img-main"
-						width="100%"
-						src={require(`../../../images/${this.state.buildData.slug}/hose.png`)}
-					/>
-				) : null}
-				{imageNumber !== "" && customText !== "" ? (
-					<div className="image-preview">
-						<p className="hose-text">{customText}</p>
-						<img
-							width="100%"
-							src={require(`../../../images/${this.state.buildData.slug}/${this.state.buildData.imageNumber}.png`)}
-						/>
-					</div>
-				) : null}
-				<p className="disclaimer">
-					&#x2a;Image for reference only. Final product may vary.
-				</p>
-				<div className="specs-wrapper">
-					<h1>
-						<span>Custom </span>
-						<br />
-						{this.state.buildData.productName}
-					</h1>
-					<div className="specs">
-						<h3 className="bold-text" style={{ marginBottom: "15px" }}>
-							SPECS:
-						</h3>
+    return (
+      <div id="product-review">
+        <div className="logo-wrapper">
+          <img src={Logo} />
+        </div>
+        {this.state.buildData.length !== 0 ? (
+          <img
+            className="img-main"
+            width="100%"
+            src={require(`../../../images/${this.state.buildData.slug}/hose.png`)}
+          />
+        ) : null}
+        {imageNumber !== "" && customText !== "" ? (
+          <div className="image-preview">
+            <p className="hose-text">{customText}</p>
+            <img
+              width="100%"
+              src={require(`../../../images/${this.state.buildData.slug}/${this.state.buildData.imageNumber}.png`)}
+            />
+          </div>
+        ) : null}
+        <p className="disclaimer">
+          &#x2a;Image for reference only. Final product may vary.
+        </p>
+        <div className="specs-wrapper">
+          <h1>
+            <span>Custom </span>
+            <br />
+            {this.state.buildData.productName}
+          </h1>
+          <div className="specs">
+            <h3 className="bold-text" style={{ marginBottom: "15px" }}>
+              SPECS:
+            </h3>
 
-						{specs}
-						<p style={{ float: "right" }}>
-							<span className="bold-text">Total Price:</span> $
-							{this.state.buildData.totalPrice}
-						</p>
-					</div>
-				</div>
-			</div>
-		);
-	}
+            {specs}
+            <p style={{ float: "right" }}>
+              <span className="bold-text">Total Price:</span> $
+              {this.state.buildData.totalPrice}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	return {
-		selectedPath: state.main.selectedPath,
-		price: state.main.price,
-		pathIndex: state.main.pathIndex
-	};
+  return {
+    selectedPath: state.main.selectedPath,
+    price: state.main.price,
+    pathIndex: state.main.pathIndex
+  };
 };
 
 export default connect(mapStateToProps)(ProductReview);
